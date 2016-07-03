c87c0916a1ad6127e6da7ef1b2767222986ff979
dion <dion@macintyres.me.uk>
2016-07-03 18:10:33 +0100

    Added ability to register users, added bcrypt, tidied up some code

--- server/Model/User.js
+++ server/Model/User.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 let userSchema = new mongoose.Schema({
   "email": { type: String, required: true },
   "password": { type: String, required: true },
-  "passwordChangeToken": { type: String, required: false },
+  "passwordChangeToken": { type: String, default: null },
   "admin": { type: Boolean, required: true }
 });
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,32 +4,56 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var User = require("../Model/User");
 var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
 
 let router = express.Router();
+const saltRounds = 13;
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
+
+
+router.post('/register', function(req, res){
+	bcrypt.hash(req.body.password, saltRounds, function(err, passwordHash){
+		var user = new User({
+			email: req.body.email.toLowerCase(),
+			password: passwordHash,
+			admin: false
+		});
+		user.save(function(err){
+			if(err){
+				console.error(err);
+			} else{
+				res.status(200).end();
+			}
+		});
+	});
+});
+
 
 router.post('/auth', function(req, res){
 	User.findOne({email: req.body.email.toLowerCase()}, function(err, user){
 		if(err){
 			console.error(err);
+			res.status(500).json({err: err});
 		} else if(!user){
-			res.json({message:"user not found", err: err});
+			res.json({err:"user not found"});
 		} else {
-			if (user.password != req.body.password){
-				res.json({message:"Incorrect password", err:err});
-			} else{
-				//Simply using the user object returned from the query adds an excessive amount
-				//of data to the payload for some reason, so I'm individually assigning stuff like
-				//{id: user.id} instead
-				var token = jwt.sign({
-					id: user.id,
-					email: user.email,
-					admin: user.admin
-				}, req.app.get('superSecret'),{expiresIn: "24h"});
-				res.json({success: true, token:token, user:user});
-			}
+			bcrypt.compare(req.body.password, user.password, function(err, match){
+				if(!match){
+					res.json({err:"Incorrect password"});
+				} else{
+					//Simply using the user object returned from the query adds an excessive amount
+					//of data to the payload for some reason, so I'm individually assigning stuff like
+					//{id: user.id} instead
+					var token = jwt.sign({
+						id:user.id,
+						email:user.email,
+						admin:user.admin
+					}, req.app.get('superSecret'),{expiresIn: "24h"});
+					res.status(200).json({token:token});
+				}
+			});
 		}
 	});
 });
