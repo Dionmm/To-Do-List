18af7c797a95843e5dc6714724cba3b88f930ff8
dion <dion@macintyres.me.uk>
2016-07-04 00:32:37 +0100

    Added password reset ability through email

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,9 +5,22 @@ var bodyParser = require("body-parser");
 var User = require("../Model/User");
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
+var nodemailer = require('nodemailer');
+var path = require('path');
 
 let router = express.Router();
 const saltRounds = 13;
+
+var smtpConfig = {
+    host: 'smtp.gmail.com',
+    auth: {
+        user: 'orangebees68@gmail.com',
+        pass: 'BigHoney68'
+    }
+};
+var transporter = nodemailer.createTransport(smtpConfig);
+
+
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -22,7 +35,7 @@ router.post('/register', function(req, res){
 		});
 		user.save(function(err){
 			if(err){
-				console.error(err);
+				res.status(500).json({err:err});
 			} else{
 				res.status(200).end();
 			}
@@ -34,10 +47,9 @@ router.post('/register', function(req, res){
 router.post('/auth', function(req, res){
 	User.findOne({email: req.body.email.toLowerCase()}, function(err, user){
 		if(err){
-			console.error(err);
 			res.status(500).json({err: err});
 		} else if(!user){
-			res.json({err:"user not found"});
+			res.json({err:"User not found"});
 		} else {
 			bcrypt.compare(req.body.password, user.password, function(err, match){
 				if(!match){
@@ -51,4 +63,99 @@ router.post('/auth', function(req, res){
 	});
 });
 
+router.post('/forgot', function(req, res){
+	//Search for the email address the user entered for the password reset
+	User.findOne({email: req.body.email.toLowerCase()}, function(err, user){
+		if(err){
+			res.status(500).json({err: err});
+		} else if(!user){
+			res.json({err:"Email address not found"});
+		} else {
+			//Check there is a connection to the email account
+			transporter.verify(function(err) {
+				if(err){
+					res.status(500).json({err:err});
+				} else{
+					//Create a unique token
+					var token = jwt.sign({email: user.email}, req.app.get('superSecret'),{expiresIn: "24h"});
+
+					transporter.sendMail({
+					from: 'orangebees68@gmail.com',
+					  to: user.email,
+					  subject: 'Password Change Request',
+					  text: 'Go fuck yourself, remember your password next time you idiot. http://localhost:3000/user/forgot/' + token
+					});
+
+					//Save the token in the DB for reference later 
+					user.passwordChangeToken = token;
+					user.save(function(err){
+			          if(err){
+			            res.status(500).json({"err": err});
+			          } else{
+			            res.status(200).end();
+			          }
+			        });
+				}
+			});
+
+
+			res.status(200).end();
+
+		}
+	});
+});
+//TODO: respond with a webpage here
+router.get('/forgot/:token', function(req, res){
+	res.sendFile(path.join(__dirname,'../../public/forgot.html'));
+	//res.json({text: "Some page here so they can reset their password, should post to the exact same url", token: req.params.token});
+});
+
+router.post('/forgot/:token', function(req, res){
+	User.findOne({passwordChangeToken: req.params.token}, function(err, user){
+		if(err){
+			res.status(500).json({err: err});
+		} else if(!user){
+			res.json({err:"Token not found"});
+		} else {
+			bcrypt.hash(req.body.password, saltRounds, function(err, passwordHash){
+				user.password = passwordHash;
+				user.passwordChangeToken = null;
+				user.save(function(err){
+					if(err){
+						res.status(500).json({err:err});
+					} else{
+						res.status(200).end();
+					}
+				});
+			});
+		}
+	});
+});
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
